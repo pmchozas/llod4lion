dbc46f219d23fa199c8188bd8df5ae204aa39ca6
pmchozas <patriciamartinchozas@gmail.com>
2020-05-16 15:56:12 +0200

    minicorpus

--- scriptbadenes/badenes_terms.py
+++ scriptbadenes/badenes_terms.py
@@ -42,12 +42,12 @@ def get_keywords(text):
 
 
 if __name__ == "__main__":
-  file = open("smallcorpus.txt", "r")
+  file = open("minicorpus.txt", "r")
   text=file.read()
 
   #print(len(text))
   
-  f = open('results.txt','a')
+  f = open('miniresults.txt','a')
   test=get_keywords(text)
   #print(test)
   f.write(str(test) + '\n')
